e7b4f966adb4c06e19d0faf22c553505505dc3da
Jahirul Islam <86736791+jahirulislm@users.noreply.github.com>
2023-02-21 21:06:32 +0600

    passwordgeneratorapp

---
+++ PasswordGenerator/App.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>A Password Generator App with Tyepscript in React native
+      </Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
